c00ac412c217108b7eb8fcb3321c278ca2eab90a
sik2 <devpang20@gmail.com>
2023-08-18 22:22:00 +0900

    로컬스토리지 구현

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Header from "./Header.js";
 import TodoForm from "./TodoForm.js";
 import TodoList from "./TodoList.js";
+import { getItem, setItem } from "./storage.js";
 
 export default function App({$target}) {
-    const initData = [
+    const initData = getItem('todos', [
         {
             id: 1,
             text: '공부하기',
@@ -19,7 +20,7 @@ export default function App({$target}) {
             text: '운동하기',
             checked: true
         }
-    ]
+    ])
 
 
     let lastId = initData[initData.length - 1].id;
@@ -37,11 +38,13 @@ export default function App({$target}) {
        todoList.setState(nextState);
        lastId++;
        console.log(todoList.state);
+       setItem('todos', JSON.stringify(nextState))
     }
 
     const onDelete = (id) => {
         const nextState = todoList.state.filter(todo => todo.id != id);
         todoList.setState(nextState)
+        setItem('todos', JSON.stringify(nextState))
     }
 
     const onToggle = (id) => {
@@ -49,6 +52,7 @@ export default function App({$target}) {
             todo.id === id ? {...todo, checked: !todo.checked} : todo
         )
         todoList.setState(nextState)
+        setItem('todos', JSON.stringify(nextState))
     }
 
     new Header({$target});
