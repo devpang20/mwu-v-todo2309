ac23b57a01d0572fef56b1d12057e1effecb44af
sik2 <devpang20@gmail.com>
2023-08-18 21:50:20 +0900

    삭제 기능 추가

--- src/App.js
+++ src/App.js
@@ -3,12 +3,43 @@ import TodoForm from "./TodoForm.js";
 import TodoList from "./TodoList.js";
 
 export default function App({$target}) {
-    const initData = ['공부하기', '밥먹기', '운동하기']
+    const initData = [
+        {
+            id: 1,
+            text: '공부하기',
+            checked: true
+        },
+        {
+            id: 2,
+            text: '밥먹기',
+            checked: false
+        },
+        {
+            id: 3,
+            text: '운동하기',
+            checked: true
+        }
+    ]
 
     const onSubmit = (text) => {
-       const nextState = [...todoList.state, text]
+       const nextState = todoList.state.concat(
+        [
+            {
+                id: 4,
+                text,
+                checked: false
+            }
+        ]
+       )
+
+
        todoList.setState(nextState);
-       console.log(todoList.state)
+       console.log(todoList.state);
+    }
+
+    const onDelete = (id) => {
+        const nextState = todoList.state.filter(todo => todo.id != id);
+        todoList.setState(nextState)
     }
 
     new Header({$target});
@@ -16,5 +47,5 @@ export default function App({$target}) {
     new TodoForm({$target, onSubmit});
  
     
-    const todoList = new TodoList({$target, initData});
+    const todoList = new TodoList({$target, initData, onDelete});
 }
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,4 +1,4 @@
-export default function TodoList({$target, initData}) {
+export default function TodoList({$target, initData, onDelete}) {
     const $list = document.createElement('div');
     $target.appendChild($list);
 
@@ -8,11 +8,22 @@ export default function TodoList({$target, initData}) {
         this.state = nextState;
         this.render();
     }
+    $list.addEventListener('click', e => {
+        if (e.target.classList.contains('del_btn')) {
+            const id = parseInt(e.target.getAttribute('data-id'))
+            onDelete(id)
+        }
+    })
 
     this.render = () => {
         $list.innerHTML = `
         <ul>
-            ${this.state.map(todo => `<li>${todo}</li>`).join('')}
+            ${this.state.map(todo => `
+            <li>
+                ${todo.id}
+                <span>${todo.text}</span>
+                <button data-id=${todo.id} class="del_btn">X</button>
+            </li>`).join('')}
         </ul>
     `
     }
